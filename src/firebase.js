1c7b3aee65964a9d9bf62be24fea1a0645d55e7e
antinglimmy <antinglim@gmail.com>
2022-08-30 20:38:21 +0800

    Try to fix.

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,10 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_STORAGE_APIKEY,
   authDomain: process.env.REACT_APP_FIREBASE_STORAGE_AUTHDOMAIN,
@@ -18,3 +21,4 @@ const app = initializeApp(firebaseConfig);
 //get the database of the firebase app that was initialised
 export const database = getDatabase(app);
 export const storage = getStorage(app);
+export const auth = getAuth(app);
